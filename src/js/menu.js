21f31135de3df149f798123a85c1987f1790de9f
MaxGraw <max@graw-online.de>
2021-12-30 18:08:02 +0100

    Created File Structure
    added postcss-custommedia
    added postcss-nesting
    created postcss config

---
+++ src/js/menu.js
@@ -0,0 +1,9 @@
+document.addEventListener("click", (event)=> {
+	if (event.target.tagName == "BUTTON") {
+		if (getComputedStyle(event.target.nextElementSibling).display === "none") {
+			return event.target.nextElementSibling.classList.remove("hidden")
+		}
+		return event.target.nextElementSibling.classList.add("hidden")
+	}
+	for (element of document.getElementsByClassName("submenu")) { element.classList.add("hidden") }
+});
